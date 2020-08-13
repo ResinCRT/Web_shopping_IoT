9437b3b55ed5fbbdb984fbe523162d65f75b6ad9
DESKTOP-CS5B0E0\i <sdc99217@naver.com>
2020-08-13 14:55:36 +0900

    install -r require.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'bookmark.apps.BookmarkConfig',
-    'blog.apps.BlogConfig',
+    # 'bookmark.apps.BookmarkConfig',
+    # 'blog.apps.BlogConfig',
 
     'taggit.apps.TaggitAppConfig',
     'taggit_templatetags2',
