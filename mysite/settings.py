eb20d5c84d131dd92607d3f30e2242a513dadb6b
poding <wusldd@naver.com>
2020-08-13 16:20:26 +0900

    지현 app shop 생성

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'shop.apps.ShopConfig',
+
     # 'bookmark.apps.BookmarkConfig',
     # 'blog.apps.BlogConfig',
 
