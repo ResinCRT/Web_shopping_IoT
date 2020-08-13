329da1551a1218af6aab1655d4dc1e88fbaeac76
DESKTOP-CS5B0E0\i <sdc99217@naver.com>
2020-08-13 16:35:37 +0900

    mypage/register add

---
+++ register/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.views.generic import CreateView, TodayArchiveView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import AccessMixin
+
+# Create your views here.
+class UserCreateView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
+
+class UserCreateDoneTV(TemplateView):
+    template_name = 'registration/register_done.html'
+
+class OwnerOnlyMixin(AccessMixin):
+    raise_exception = True
+    permission_denied_message = "Owner only can update/ delete the object"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.request.user != self.object.owner:
+            self.handle_no_permission()
+        return super().get(request, *args, **kwargs)
