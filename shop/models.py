94585804febda0c3e68dc9fa0a433ecd6f4dabed
poding <wusldd@naver.com>
2020-08-13 18:17:12 +0900

    지현 SHOP MODEL에 옮김

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.IntegerField(primary_key=True)
+    p_name = models.CharField(max_length=45, blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    description = models.CharField(max_length=45, blank=True, null=True)
+    read_cnt = models.IntegerField(blank=True, null=True)
+    category_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'product'
+        unique_together = (('product_id', 'category_category_id'),)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# 상품 목록 출력
+class ProductLV(ListView):
+    model = Product
+    template_name = 'shop/procduct_all.html'
+    context_object_name = 'products'
+    # 한페이지에 보여줄 문서 건수는 상의하기
+
+# 상품 상세 보기 출력
+class ProductDV(DetailView):
+    model = Product
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.get_object
