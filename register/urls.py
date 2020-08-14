6773a744ebca95fe8e232a8a60f11f50f1337130
DESKTOP-CS5B0E0\i <sdc99217@naver.com>
2020-08-14 14:10:42 +0900

    practice

--- register/urls.py
+++ register/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path
+from register.views import UserCreateView, UserCreateDoneTV
 
 app_name = 'register'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    # path('register/', name='sign_up'),
-    # path('register/done/', name='sign_up_done'),
+    path('register/', UserCreateView.as_view(), name='sign_up'),
+    path('register/done/', UserCreateDoneTV.as_view(), name='sign_up_done'),
 ]
