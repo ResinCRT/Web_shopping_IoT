f8c104e0686f5bd1de0c5aeb2d3c3c410c2beda4
kkc926 <69613189+kkc926@users.noreply.github.com>
2020-08-14 13:41:58 +0900

    model 수정

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView
+from shop.models import Product
+from django.urls import reverse_lazy
+from django.utils import timezone
 
-# Create your views here.
+
+# class PostCreateView(LoginRequiredMixin, CreateView):
+#     model = Product
+#     fields = ['title', 'description', 'content', 'tags']
+#     # initial = {'slug': 'auto-filling-do-not-input'}
+#     success_url = reverse_lazy('blog:index')
+#
+#     def form_valid(self, form):
+#         form.instance.owner = self.request.user
+#         form.instance.modify_dt = timezone.now()
+#         response = super().form_valid(form)
+#
+#         files = self.request.FILES.getlist("files")
+#         for file in files:
+#             attach_file = PostAttachFile(post=self.object, filename=file.name,
+#                                          size=file.size, content_type=file.content_type, upload_file=file)
+#             attach_file.save()
+#         return response
