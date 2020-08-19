5714eb68d5b0f0c45bf7418ec50b9e448cbcdc7a
DESKTOP-CS5B0E0\i <sdc99217@naver.com>
2020-08-19 16:19:45 +0900

    mypage view

--- mypage/views.py
+++ mypage/views.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from shop.models import Order
+from django.db.models import Q
 
 
 # Create your views here.
@@ -16,7 +18,10 @@ class ModifyUserView(LoginRequiredMixin, TemplateView):
 
 class MyOrderView(LoginRequiredMixin, ListView):
     template_name = "mypage_order.html"
-    success_url = reverse_lazy('mypage:mypage_index')
+    model = Order
+    paginate_by = 3
+    def get_queryset(self):
+        return Order.objects.filter(user_id=self.request.user.pk)
 
 class MyReviewView(LoginRequiredMixin, ListView):
     template_name = "mypage_review.html"
