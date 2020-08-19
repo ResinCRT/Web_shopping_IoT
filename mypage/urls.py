6433a5060473ea592d92a1044133dda874f3242f
DESKTOP-CS5B0E0\i <sdc99217@naver.com>
2020-08-19 14:57:53 +0900

    mypage 템플릿 초안 + 마이페이지 뷰

--- mypage/urls.py
+++ mypage/urls.py
@@ -4,9 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.mixins import AccessMixin
 from django.views.defaults import permission_denied
-from mysite.views import HomeView
-import datetime
+from mypage.views import *
+
+app_name = "mypage"
 
 urlpatterns = [
+    path('', MypageView.as_view(), name="mypage_index"),
+    path('modify/', ModifyUserView.as_view(), name="modify_user"),
 
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class MypageView(LoginRequiredMixin, TemplateView):
+    template_name = "mypage_main.html"
+
+
+class ModifyUserView(LoginRequiredMixin, TemplateView):
+    template_name = "mypage_modify_user.html"
+    success_url = reverse_lazy('mypage:mypage_index')
+
