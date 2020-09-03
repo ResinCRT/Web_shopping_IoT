95b737a3cc4d93e878a2c0b4b652ea975bb99ebc
Juho <hatedbs99@gmail.com>
2020-09-03 13:11:01 +0900

    cart commit

--- cart/urls.py
+++ cart/urls.py
@@ -6,9 +6,14 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    # Example: /cart/
-    path('add/<int:product_id>/', views.add_cart, name='add_cart'),
     # Example: /order/
     path('', views.cart_detail, name='cart_detail'),
+    # Example: /cart/add/<int:product_id>
+    path('addtocart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    # Example: /cart/minus/<int:product_id>
+    path('add/<int:product_id>/', views.add_cart, name='add_cart'),
+    # Example: /cart/minus/<int:product_id>
+    path('minus/<int:product_id>/', views.minus_cart, name='minus_cart'),
+    # Example: /cart/remove_cart/<int:product_id>
     path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
 ]
